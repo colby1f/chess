773b80f2924d144c13295b3d6b775c21458fe5d8
Colby Fox <colbyfox@byu.edu>
2026-09-15 19:39:21 -0600

    implemented reset board

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -43,6 +43,26 @@ public class ChessBoard {
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
+        board = new ChessPiece[8][8];
+
+        ChessPiece.PieceType[] first_row = {
+            ChessPiece.PieceType.ROOK,
+            ChessPiece.PieceType.KNIGHT,
+            ChessPiece.PieceType.BISHOP,
+            ChessPiece.PieceType.QUEEN,
+            ChessPiece.PieceType.KING,
+            ChessPiece.PieceType.BISHOP,
+            ChessPiece.PieceType.KNIGHT,
+            ChessPiece.PieceType.ROOK,
+        };
+
+        for (int col = 0; col < 8; col++){
+            board[0][col] = new ChessPiece(ChessGame.TeamColor.WHITE, first_row[col]);
+            board[7][col] = new ChessPiece(ChessGame.TeamColor.BLACK, first_row[col]);
+
+            board[1][col] = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+            board[6][col] = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.PAWN);
+        }
 
     }
 
